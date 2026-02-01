c67b1f31ba75f14191465d6df70bc73e29817376
thurr-dev <arthurfern89@gmail.com>
2026-02-01 14:46:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,7 @@
-const CACHE_NAME = 'energia-v3';
-const assets = [
-  './',
-  './index.html',
-  'https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      // Baixamos os arquivos normais
-      cache.addAll(assets);
-      // Baixamos a imagem do Imgur separadamente com modo 'no-cors'
-      return cache.add(new Request('https://static.todamateria.com.br/upload/ti/po/tipos-de-energia-og.jpg?class=ogImageSquare.png', {mode: 'no-cors'}));
-    })
-  );
-  self.skipWaiting();
+const CACHE_NAME = 'energia-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
-});
-
