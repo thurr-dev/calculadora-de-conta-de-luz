b500e96e236e08443ca5ce6aa34ce26552403aad
thurr-dev <arthurfern89@gmail.com>
2026-02-01 11:52:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
 const CACHE_NAME = 'energia-v1';
 const assets = [
-  const assets = [
   './',
   './index.html',
-  'https://imgur.com/M1cU0lD.png',
-  'https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
+  './manifest.json'
 ];
 
+// Instalação: Salva arquivos essenciais no cache
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Ativação: Limpa caches antigos se houver
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
 
+// Fetch: Tenta buscar no cache, se não tiver, vai na rede
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
