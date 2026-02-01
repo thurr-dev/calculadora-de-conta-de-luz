7bd78650e8e8e36928b6158004d0692e19bd7994
thurr-dev <arthurfern89@gmail.com>
2026-02-01 15:10:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
-const CACHE_NAME = 'energia-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
+const CACHE_NAME = 'energia-v4';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/2814/2814571.png',
+  'https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
