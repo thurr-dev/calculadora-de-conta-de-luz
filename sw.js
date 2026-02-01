d6276060c7ff7168f888635b80ea23f6663e6f46
thurr-dev <arthurfern89@gmail.com>
2026-02-01 14:34:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'energia-v2';
+const CACHE_NAME = 'energia-v3';
 const assets = [
   './',
   './index.html',
@@ -6,9 +6,31 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // Baixamos os arquivos normais
+      cache.addAll(assets);
+      // Baixamos a imagem do Imgur separadamente com modo 'no-cors'
+      return cache.add(new Request('https://i.imgur.com/IiCxCzg.png', {mode: 'no-cors'}));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
